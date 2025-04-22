c57d76f8ef0307fcabca56f0fb1a912df8fa1ae5
Lizzie Salmon <144809798+lizsalmon@users.noreply.github.com>
2025-04-22 10:30:56 +0100

    Added in on screen up as a skip

--- main.ts
+++ main.ts
@@ -1,15 +1,24 @@
-input.onGesture(Gesture.Shake, function () {
-    // If the index is less than how big the array is.  This ends the game at the max number of words.
+input.onGesture(Gesture.ScreenUp, function () {
+    if (index < array_of_words.length) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.slide), music.PlaybackMode.UntilDone)
+        led.stopAnimation()
+        index += 1
+    }
+})
+input.onGesture(Gesture.ScreenDown, function () {
     if (index < array_of_words.length) {
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
-        // This stops the word displaying across the screen, if it is already completed
         led.stopAnimation()
-        // Add 1 to the index so that they are now looking at the next word
         index += 1
+        score += 1
     }
 })
 let index = 0
 let array_of_words: string[] = []
+basic.showNumber(3)
+basic.showNumber(2)
+basic.showNumber(1)
+basic.clearScreen()
 array_of_words = [
 "cat",
 "dog",
@@ -21,19 +30,16 @@ array_of_words = [
 ]
 let timer = 30
 index = 0
+let score = 0
 loops.everyInterval(1000, function () {
-    // Counting down the time every second
     timer += -1
 })
 basic.forever(function () {
-    // less than or equal to as the time will keep going down after zero
     if (timer <= 0) {
-        // If the time has run out then show the score at that point
-        basic.showNumber(index)
+        basic.showNumber(score)
     } else if (index >= array_of_words.length) {
-        basic.showNumber(index)
+        basic.showNumber(score)
     } else {
-        // Show the current word that they're on
         basic.showString("" + (array_of_words[index]))
     }
 })
